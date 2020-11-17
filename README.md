401fa43131df542c39adda9b906278621538cc15
daofeng.du <daofeng.du@boonray.com>
2020-11-17 22:25:29 +0800

    ros installation script

---
+++ README.md
@@ -0,0 +1,10 @@
+# zxy_ADAS
+
+This is a script for installing the ros-kinetic tool in ubuntu16.04 environment.
+
+Environment:
+ubuntu16.04
+
+Installation steps:
+sh ros_install.sh
+
---
+++ ros_install.sh
@@ -0,0 +1,19 @@
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+
+sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
+
+#sudo apt-key adv --keyserver  hkp://pgp.mit.edu:80  --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
+
+sudo apt-get update
+
+sudo apt-get install ros-kinetic-desktop-full
+
+sudo rosdep init
+
+rosdep update
+
+echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
+
+source ~/.bashrc
+
+sudo apt-get install python-rosinstall python-rosinstall-generator python-wstool build-essential
